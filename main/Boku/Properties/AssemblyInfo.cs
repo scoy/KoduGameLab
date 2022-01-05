a12060bad5982d856f7a24af4a096bfdec4ce9aa
scoy <scoy@infiniteinstant.com>
2022-01-05 14:32:18 -0800

    Build 1.6.11.0

--- main/Boku/Properties/AssemblyInfo.cs
+++ main/Boku/Properties/AssemblyInfo.cs
@@ -38,5 +38,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 // This build number is auto-incremented by BokuBuildTool.exe
-[assembly: AssemblyVersion("1.6.10.0")]
+[assembly: AssemblyVersion("1.6.11.0")]
  
