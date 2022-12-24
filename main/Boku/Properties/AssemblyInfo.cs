9189eac466e4cdf604321f095deeac491447ee55
scoy <scoy@infiniteinstant.com>
2022-12-23 20:33:53 -0800

    Build 1.6.14.0
    Build for University of Indiana work.

--- main/Boku/Properties/AssemblyInfo.cs
+++ main/Boku/Properties/AssemblyInfo.cs
@@ -38,5 +38,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 // This build number is auto-incremented by BokuBuildTool.exe
-[assembly: AssemblyVersion("1.6.13.0")]
+[assembly: AssemblyVersion("1.6.14.0")]
  
